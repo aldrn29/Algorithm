7af240d7cc61bbeb945e2be99a0900f30d3c3cd2
aldrn <aldrn29@gmail.com>
2021-08-27 19:13:00 +0900

    backtracking

--- Beakjoon/백트레킹/2529 [부등호].py
+++ Beakjoon/백트레킹/2529 [부등호].py
@@ -1,3 +1,7 @@
+###
+# permutations
+###
+
 from sys import stdin
 from itertools import permutations
 
@@ -17,6 +21,50 @@ for i in num :
             continue
         else : break
 
-# 앞에 0 추가하기
+str_max = str(max_num)
+str_min = str(min_num)
+
+if len(str_max) != k+1 : str_max = '0' + str_max
+if len(str_min) != k+1 : str_min = '0' + str_min
+
+print(str_max)
+print(str_min)
+
+
+###
+# backtracking 
+###
+
+from sys import stdin
+
+k = int(input())
+s_list = list(map(str, stdin.readline().split())) 
+
+visited = [0]*10
+max_num = ""
+min_num = ""
+
+def check(i, j, sign) :
+    if sign =='<' :
+        return i<j
+    else: return i>j
+
+def solve(idx, s) :
+    global max_num,min_num
+
+    if idx == k+1 :
+        if len(min_num) == 0 :
+            min_num = s
+        else : max_num = s
+        return
+
+    for i in range(10) : 
+        if visited[i] == 0 :
+            if idx == 0 or check(s[-1], str(i), s_list[idx]) :
+                visited[i] = True
+                solve(idx+1, s+str(i))
+                visited[i] = False
+
+solve(-1, "")
 print(max_num)
 print(min_num)
