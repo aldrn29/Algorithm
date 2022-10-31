ddb02ceda0c978717095a70c1a50eaba9d8baf1f
aldrn <aldrn29@gmail.com>
2022-10-31 20:26:16 +0900

    21920 서로소 평균

--- Beakjoon/수학/21920 [서로소 평균].py
+++ Beakjoon/수학/21920 [서로소 평균].py
@@ -1,27 +1,51 @@
-from re import I
-from sys import stdin
-input = stdin.readline
+# from sys import stdin
+# input = stdin.readline
+
+# n = int(input())
+# a = list(map(int, input().split()))
+# x = int(input())
+
+# def getDivisor(num) :
+#     divisorList = set()
+#     for i in range(2, num // 2 +1) :
+#         if num % i == 0 :
+#             divisorList.add(i)
+
+#     divisorList.add(num)
+#     return divisorList
+
+# result = []
+# xList = getDivisor(x) 
+
+# for i in a :
+#     if len(xList & getDivisor(i)) > 0 :
+#         continue
+#     else : result.append(i)
+
+# print(sum(result) // len(result))
 
 n = int(input())
 a = list(map(int, input().split()))
 x = int(input())
+result = 0
+cnt = 0
 
 def getDivisor(num) :
-    divisorList = set()
-    for i in range(2, num // 2 +1) :
+    divisorList = []
+    for i in range(1, int(num**0.5)+1) :
         if num % i == 0 :
-            divisorList.add(i)
+            divisorList.extend([i, num//i])
+    divisorList.remove(1)
+    return list(set(divisorList))
 
-    divisorList.add(num)
-    return divisorList
+xList = getDivisor(x)
 
-result = []
-xList = getDivisor(x) 
+for num in a :
+    for x in xList : # 4의 약수 [2 ,4]
+        if num % x == 0 : # [4,2,8,5,7] 이 4의 약수(2)로 나뉘면
+            break
+    else :
+        result += num
+        cnt += 1
 
-for i in a :
-    if len(xList & getDivisor(i)) > 0 :
-        continue
-    else : result.append(i)
-
-print(result)
-print(sum(result) // len(result))
+print(result / cnt)
