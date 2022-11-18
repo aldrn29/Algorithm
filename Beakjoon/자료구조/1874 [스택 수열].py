fd9704414a256cf60b96757474867b74d038ae6e
aldrn <aldrn29@gmail.com>
2022-11-18 18:04:17 +0900

    1874 스택 수열

--- Beakjoon/자료구조/1874 [스택 수열].py
+++ Beakjoon/자료구조/1874 [스택 수열].py
@@ -1,8 +1,27 @@
-n = int(input())
-m = [int(input()) for _ in range(n)]
+from collections import deque
 
-# 입력받은 수가 l에 없다면, ~m까지 추가하고 + 출력
-# 근데 다음에 입력 받은 수가 바로 전에 입력 받은 것보다 크다면, 그 전꺼 빼고, - 출력
-# 있다면, m~ 까지 빼고 - 출력 
+# m 리트스대로..
+# l에 없다면 그 숫자만큼 추가한 뒤 pop
+# 마지막 숫자를 pop 하는데, 그 숫자가 아니면 NO
+def check() :
+    n = int(input())
+    m = [int(input()) for _ in range(n)]
+    l = deque()
+    count = 0
+    result = ""
 
-# NO
+    for i in m :
+        while count < i :
+            count += 1
+            l.append(count)
+            result += '+\n'
+        
+        if l[-1] == i :
+            l.pop()
+            result += '-\n'
+        else :
+            return "NO"
+
+    return result
+
+print(check())
