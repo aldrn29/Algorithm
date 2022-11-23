ed0b48fe85c3b03723509bf65645448be2d810c6
aldrn <aldrn29@gmail.com>
2022-11-23 18:28:05 +0900

    4659 비밀번호 발음하기

--- Beakjoon/문자열/4659 [비밀번호 발음하기].py
+++ Beakjoon/문자열/4659 [비밀번호 발음하기].py
@@ -1,26 +1,59 @@
-'''
-모음(a,e,i,o,u) 하나를 반드시 포함하여야 한다.
-모음이 3개 혹은 자음이 3개 연속으로 오면 안 된다.
-같은 글자가 연속적으로 두번 오면 안되나, ee 와 oo는 허용한다.
-'''
 import re
 
-def check(str) :
+
+def check(s) :
+    v = "aeiou"
     p = re.compile('[a|e|i|o|u]')
-    m = p.match(str)
+    m = p.search(s)
     
-    if m != None :
-        p = re.compile('[a|e|i|o|u]{3}|^[a|e|i|o|u]{3}')
-        m = p.match(str)
-        
-        # 같은 글자가 연속적으로 두번 오면 안되나, ee 와 oo는 허용한다.
-        # if m != None :
-            
+    # 1. 모음 하나를 반드시 포함하는 경우
+    if m == None :
+        return False
+    else :
+        # 모음, 자음이 연속으로 오는 경우
+        for i in range(len(s) - 2) :
+            # 모음인 경우
+            if s[i] in v :
+                if s[i+1] in v :
+                    if s[i+2] in v :
+                        return False
+            # 자음인 경우
+            elif s[i] not in v :
+                if s[i+1] not in v :
+                    if s[i+2] not in v :
+                        return False
 
-    return False
+        # 같은 글자가 연속적으로 두번 오는 경우
+        for i in range(len(s) - 1) :
+            if s[i] == s[i+1] and s[i] != 'e' and s[i] != 'o' :
+                return False
 
-str = input()
-if check(str) :
-    print("<", str, "> is acceptable.", sep='')
-else :
-    print("<", str, "> is not acceptable.", sep='')
+    return True
+
+while True :
+    str = input()
+    if str != 'end' :
+        if check(str) :
+            print("<", str, "> is acceptable.", sep='')
+        else :
+            print("<", str, "> is not acceptable.", sep='')
+    else :
+        break
+
+    
+'''
+while True :
+    str = input()
+    if str == 'end' :
+        break
+
+    case1 = len(re.findall('[aeiou]', str)) != 0 
+    case2 = len(re.findall('[aeiou]{3}|[^aeiou]{3}', str)) == 0
+    case3 = len(re.findall('([a-df-np-z])\\1', str)) == 0
+
+
+    if case1 and case2 and case3:
+        print(f'<{str}> is acceptable.')
+    else:
+        print(f'<{str}> is not acceptable.')
+'''
